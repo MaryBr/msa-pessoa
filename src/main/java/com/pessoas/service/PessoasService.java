30998cd55dbd97fb8e15b065e3f61e815a4d6799
marina neri <marina_neri@hotmail.com>
2021-01-25 20:52:18 -0300

    correção service com a adição do dozerConverter

--- src/main/java/com/pessoas/service/PessoasService.java
+++ src/main/java/com/pessoas/service/PessoasService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pessoas.data.model.Pessoas;
+import com.pessoas.data.vo.PessoasVO;
+import com.pessoas.dozerConverter.dozerConverter;
 import com.pessoas.exception.ResourceNotFoundException;
 import com.pessoas.repository.PessoasRepository;
 
@@ -19,25 +21,29 @@ public class PessoasService {
 
 
 	
-	public Pessoas execute(Pessoas pessoas) {
+	public PessoasVO execute(PessoasVO pessoas) {
 		/* O controller chama o execute que é o metodo que contem a logica que vai até a base de dados e 
 		 * depois o resultado é retornado para o controller que é enviado para o JSon para o client
 		 * */
-		return pessoasRepository.save(pessoas);
+		Pessoas entidade = dozerConverter.parseObject(pessoas, Pessoas.class);
+		PessoasVO vo = dozerConverter.parseObject(pessoasRepository.save(entidade), PessoasVO.class);
+		return vo;
 	}
 	
-	public List<Pessoas> findAll (){			
-		return pessoasRepository.findAll();
+	public List<PessoasVO> findAll (){			
+		return dozerConverter.parseListObject(pessoasRepository.findAll(), PessoasVO.class);
 	}
 	
-	public Pessoas findById(Long id) {
+	public PessoasVO findById(Long id) {
 				
-		return  pessoasRepository.findById(id).
+		Pessoas vo = pessoasRepository.findById(id).
 				orElseThrow(()->new ResourceNotFoundException("Não foi encontrados registros para esse id"));
+		
+		return  dozerConverter.parseObject(vo, PessoasVO.class);
 	}
 	
 
-	public Pessoas executeUpdate(Pessoas p) {
+	public PessoasVO executeUpdate(PessoasVO p) {
 		
 		Pessoas pessoa = pessoasRepository.findById(p.getId()).
 				orElseThrow(()->new ResourceNotFoundException("Não foi encontrados registros para esse id"));
@@ -46,7 +52,10 @@ public class PessoasService {
 		pessoa.setUltimoNome(p.getUltimoNome());
 		pessoa.setEndereco(p.getEndereco());
 		pessoa.setSexo(p.getSexo());
-		return pessoasRepository.save(pessoa);
+		
+		PessoasVO vo = dozerConverter.parseObject(pessoasRepository.save(pessoa), PessoasVO.class);
+		
+		return vo;
 	}
 	
 	
